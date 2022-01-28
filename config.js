da19a612ccc13f01d084c94e48fd3a2c57c2113a
李泽辉 <49223294+lLize-huil@users.noreply.github.com>
2022-01-28 14:49:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   fromDisplayText: '李泽辉', // 收件箱展示的来件人名字
   fromDisplaySubText: '来自辉哥的爱心邮件', // 收件箱展示的次级标题
   user: '1841632321@qq.com', // 发送者邮箱
-  pass: 'icuqdltxvwmpdegg', // 发送者邮箱MTP协议密码
+  pass: 'dxozvddewcsxbiab', // 发送者邮箱MTP协议密码
   to: '643695314@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
   location: '101050907',  // 
