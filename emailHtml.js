70a9974a0473a40e7cd19d400663079d304eaa58
李泽辉 <49223294+lLize-huil@users.noreply.github.com>
2022-02-03 07:55:25 +0800

    Update emailHtml.js

--- emailHtml.js
+++ emailHtml.js
@@ -53,7 +53,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays, logDays) {
               ${daily[0].text}
             </li>
             <li style="margin-bottom: 10px">
-              合情，合理，合法，和你，我爱你~~
+              合情，合理，合法，和你，我爱你哦~~
             </li>
           </ul>
         </div>
