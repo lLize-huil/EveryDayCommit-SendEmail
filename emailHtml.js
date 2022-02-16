0433a552b78f05afcf3b7669912ed00977ea20a8
李泽辉 <49223294+lLize-huil@users.noreply.github.com>
2022-02-16 11:08:38 +0800

    Update emailHtml.js

--- emailHtml.js
+++ emailHtml.js
@@ -56,7 +56,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays, logDays) {
               贴心小提示：可以边听歌，边看冰墩墩哦，点击下方链接，然后返回邮件界面再点击冰墩墩链接就可以啦！冰墩墩界面可以进行放大缩小哦~
             </li>
             <li style="margin-bottom: 10px">
-              <a href="https://music.163.com/song?id=1407789605&userid=616425958">点这来看看今天是什么音乐叭</a>
+              <a href="https://music.163.com/song?id=456167591&userid=616425958">这首歌不是写我呢嘛哈哈</a>
             </li>
             <li style="margin-bottom: 10px">
               <a href="http://www.zhangzhiyu.live:8900/3d/">冰墩墩这么火，别人有的你当然也要有，才不会让你羡慕别人呢~断货买不到就送你一个代码冰墩墩好啦~</a>
