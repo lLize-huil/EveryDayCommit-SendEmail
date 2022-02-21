0d54ed24fe3ae6f112dde7f894b7b7c731dc5930
李泽辉 <49223294+lLize-huil@users.noreply.github.com>
2022-02-21 09:31:27 +0800

    Update emailHtml.js

--- emailHtml.js
+++ emailHtml.js
@@ -56,7 +56,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays, logDays) {
               贴心小提示：冰墩墩和雪融融没法缩放哦~觉得视角小可以用平板观看
             </li>
             <li style="margin-bottom: 10px">
-              <a href="https://music.163.com/song?id=541687281&userid=616425958">慢慢喜欢你，慢慢的亲密，慢慢聊自己，慢慢和你走在一起~</a>
+              <a href="https://music.163.com/song?id=27731339&userid=616425958">你的出现，让别人都显得不过如此。</a>
             </li>
             <li style="margin-bottom: 10px">
               <a href="https://dragonir.github.io/3d/#/olympic">冰墩墩和雪融融~</a>
