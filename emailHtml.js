2c9e151b3bea151adfeba69eccf7ea3a61e01d78
李泽辉 <49223294+lLize-huil@users.noreply.github.com>
2022-03-09 17:56:24 -0800

    Update emailHtml.js

--- emailHtml.js
+++ emailHtml.js
@@ -56,7 +56,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays, logDays) {
               贴心小提示：冰墩墩和雪融融没法缩放哦~觉得视角小可以用平板观看
             </li>
             <li style="margin-bottom: 10px">
-              <a href="https://music.163.com/song?id=450222687&userid=616425958">林俊杰yyds！</a>
+              <a href="https://music.163.com/#/song?id=1894094482&userid=616425958">这几天你不怎么看消息也不怎么回我，秋涵，我很想你</a>
             </li>
             <li style="margin-bottom: 10px">
               <a href="https://dragonir.github.io/3d/#/olympic">冰墩墩和雪融融~</a>
